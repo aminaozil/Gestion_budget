991df3a32038063f88f3d3f3c2a77eae18b62c25
aminaozil <youmaminatambengue@gmail.com>
2024-11-21 00:41:18 +0000

    ajouter et suppeimer revenu

--- app.py
+++ app.py
@@ -22,23 +22,34 @@ class Depense(db.Model):
     def __repr__(self):
         return f"Gestion_budget: {self.titre, self.montant}"
 
+
 #liste page index pour depense
 @app.route("/")
 def index():
-
-    page = request.args.get('page', 1, type=int)  # Default to page 1 if not specified
+    page = request.args.get('page', 1, type=int)  # par defaut 1
     #variable depenses pour listes les depenses par ordre 
     depenses = Depense.query.order_by(Depense.titre).paginate(page=page, per_page=5, error_out=False)
+    page = request.args.get('page', 1, type=int)
+    revenus = Revenu.query.order_by(Revenu.titre).paginate(page=page, per_page=5, error_out=False)
+
     #calcul de la somme des depenses on récupère tout les dep puis on util la fonc sum pour faire la somme 
     # et scalar() pour renvoyer une seule valeur
     total_deps = db.session.query(db.func.sum(Depense.montant)).scalar()
+    budget = db.session.query(db.func.sum(Revenu.montant)).scalar()
+    solde = total_deps - budget
 
-    return render_template("index.html", depenses=depenses, total_deps=total_deps)
+    return render_template("index.html",total_deps=total_deps, depenses=depenses, revenus=revenus, budget=budget, solde=solde)
+
+#index pour depense
+@app.route("/depense")
+def index_depense():
+   
+    return render_template("depense/index_depense.html")
 
 #create depense
 @app.route("/create_dep", methods=["GET","POST"])
 def create_dep():
-    error =None
+
     if request.method == "POST":
         titre = request.form['titre']
         montant = request.form['montant']
@@ -47,7 +58,7 @@ def create_dep():
         try:
             db.session.add(dep)
             db.session.commit()
-            error="Ivalid"
+            
             return redirect("/")
         except Exception:
             return "erreure "
@@ -55,7 +66,7 @@ def create_dep():
         flash("Ajout fait avec success", "success")
         redirect("/")
 
-    return render_template("create_depense.html",error=error)
+    return render_template("depense/create_depense.html")
 
 
 @app.route("/delete/<int:id>/")
@@ -85,7 +96,50 @@ def update(id):
         except Exception:
             print("erreur")
     
-    return render_template("update.html", depense=depense)
+    return render_template("depense/update.html", depense=depense)
+
+################################################
+#la classe revenu creation des tables
+class Revenu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(100), nullable=False)
+    montant = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Gestion_budget: {self.titre, self.montant}"
+
+#index revenu
+@app.route("/revenu")
+def index_revenu():
+
+    return render_template("revenu/index_revenu.html")
+
+#supprimer revenu
+@app.route("/delete_revenu/<int:id>/")
+def delete_revenu(id):
+    revenus = Revenu.query.get_or_404(id)
+    try:    
+        db.session.delete(revenus)
+        db.session.commit()
+        return redirect("/")
+    except Exception:
+        print("invalid")
+
+#ajouter revenu
+@app.route("/create_revenu", methods=["GET", "POST"])
+def create_revenu():
+    if request.method == "POST":
+        titre = request.form['titre']
+        montant = request.form['montant']
+        try:
+            rev=Revenu(titre=titre, montant=montant)
+            db.session.add(rev)
+            db.session.commit()
+            return redirect("/")
+        except Exception:
+            print("une s'est produite")
+        
+    return render_template("revenu/create_revenu.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
