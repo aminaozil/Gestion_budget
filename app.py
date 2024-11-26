b7fd3de77f291c55b650af866d91f2562b8d555a
aminaozil <youmaminatambengue@gmail.com>
2024-11-26 18:16:35 +0000

    test deploiement

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, redirect,render_template, request, url_for
 from sqlalchemy import func
+# Package Python pour l'interfaçage avec des bases de données
 from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 
@@ -22,15 +23,30 @@ class Depense(db.Model):
         return f"Gestion_budget: {self.titre_depense, self.montant_depense}"
 
 #liste page index pour depense
-@app.route("/index")
-def index():
-    return render_template("index.html")
-
 @app.route("/")
+def index():
+    page = request.args.get('page', 1, type=int)  # par defaut 1
+    #variable depenses pour listes les depenses par ordre 
+    depenses = Depense.query.order_by(Depense.titre_depense).paginate(page=page, per_page=5, error_out=False)
+    page = request.args.get('page', 1, type=int)
+    revenus = Revenu.query.order_by(Revenu.titre_revenu).paginate(page=page, per_page=5, error_out=False)
+     #calcul de la somme des depenses on récupère tout les dep puis on util la fonc sum pour faire la somme 
+    # et scalar() pour renvoyer une seule valeur
+    total_deps = db.session.query(db.func.sum(Depense.montant_depense)).scalar()
+    budget = db.session.query(db.func.sum(Revenu.montant_revenu)).scalar()
+    solde = budget - total_deps
+
+    return render_template("index.html",total_deps=total_deps, budget=budget, solde=solde, depenses=depenses, revenus=revenus, page=page)
+    
+
+@app.route("/depense")
 def index_depense():
-    depenses = Depense.query.order_by(Depense.titre_depense)
+    
+    page = request.args.get('page', 1, type=int)  # par defaut 1
+    #variable depenses pour listes les depenses par ordre 
+    depenses = Depense.query.order_by(Depense.titre_depense).paginate(page=page, per_page=5, error_out=False)
    
-    return render_template("/depense/index_depense.html", depenses=depenses)
+    return render_template("/depense/index_depense.html", depenses=depenses, page=page)
 
 #create depense
 @app.route("/create_dep", methods=["GET","POST"])
@@ -83,6 +99,71 @@ def update(id):
             print("erreur")
     
     return render_template("/depense/update.html", depense=depense)
+@app.route("/impo_dep")
+def impo_dep():
+   
+    return render_template("/depense/impo_dep.html")
+
+
+#la classe revenu creation des tables
+class Revenu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titre_revenu = db.Column(db.String(100), nullable=False)
+    montant_revenu = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Gestion_budget: {self.titre_revenu, self.montant_revenu}"
+
+#index revenu
+@app.route("/revenu")
+def index_revenu():
+    page = request.args.get('page', 1, type=int)
+    revenus = Revenu.query.order_by(Revenu.titre_revenu).paginate(page=page, per_page=5, error_out=False)
+
+    return render_template("/revenu/index_revenu.html", revenus=revenus, page=page)
+
+#supprimer revenu
+@app.route("/delete_revenu/<int:id>/")
+def delete_revenu(id):
+    revenus = Revenu.query.get_or_404(id)
+    try:    
+        db.session.delete(revenus)
+        db.session.commit()
+        flash("Supression fait avec success", "success")
+        return redirect("/revenu")
+    except Exception:
+        print("invalid")
+
+#ajouter revenu
+@app.route("/create_revenu", methods=["GET", "POST"])
+def create_revenu():
+    if request.method == "POST":
+        titre_revenu = request.form['titre_revenu']
+        montant_revenu = request.form['montant_revenu']
+        try:
+            rev=Revenu(titre_revenu=titre_revenu, montant_revenu=montant_revenu)
+            db.session.add(rev)
+            db.session.commit()
+            flash("Ajout fait avec success", "success")
+            return redirect("/revenu")
+        except Exception:
+            print("une s'est produite")
+        
+    return render_template("/revenu/create_revenu.html")
+# modifier revenu
+@app.route("/update_revenu/<int:id>/", methods=["GET", "POST"])
+def update_revenu(id):
+    revenu = Revenu.query.get_or_404(id)
+    if request.method == "POST":
+        revenu.titre_revenu = request.form['titre_revenu']
+        revenu.montant_revenu = request.form['montant_revenu']
+        try:
+            db.session.commit()
+            flash("Modification fait avec success", "success")
+            return redirect("/revenu")
+        except Exception:
+            print("erreur est pro")
+    return render_template("/revenu/update_revenu.html", revenu=revenu)
 
 if __name__ == "__main__":
     app.run(debug=True)
